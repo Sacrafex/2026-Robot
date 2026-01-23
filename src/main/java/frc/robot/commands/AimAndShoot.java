76f15b45387cc68f769382bd4a5e02b4661fd9d1
0xlogindae <altdaedalus@gmail.com>
2026-01-23 14:07:49 -0800

    Update AimAndShoot.java

--- src/main/java/frc/robot/commands/AimAndShoot.java
+++ src/main/java/frc/robot/commands/AimAndShoot.java
@@ -1,9 +1,6 @@
 package frc.robot.commands;
 
 import com.ctre.phoenix6.swerve.SwerveRequest;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 // import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -22,34 +19,24 @@ public class AimAndShoot extends Command {
     private final Intake intake;
     private final OuttakeAngle outtakeAngle;
     private final BooleanSupplier autoButton;
-    private final double deadbandDeg;
     private final CANdleSubsystem lights;
 
     private final SwerveRequest.RobotCentric m_request = new SwerveRequest.RobotCentric();
-    
-    private final PIDController turnPID = new PIDController(0.2, 0.0, 0.3);
 
     private static final double DEAD_BAND = 4;
-    private static final double ANGLE_MIN = 20.0;
-    private static final double ANGLE_MAX = 60.0;
-    private static final double SPEED_MIN = 0.35; 
-    private static final double SPEED_MAX = 0.8; 
-    private static final double SHOOT_DISTANCE = 12.0;
+    private static final double kP = 0.6;
+    private static final double intakeSpeed = 0.5;
 
+    // Meters
     private static final double BLUE_HOPPER_X = 4.626;
     private static final double BLUE_HOPPER_Y = 8.069;
     private static final double RED_HOPPER_X = 11.834;
     private static final double RED_HOPPER_Y = 8.069;
 
+    private static double omega = 0.0;
+
     private static boolean autoIntakeLightsEnabled = true;
-
-    private Pose2d lastKnownPose = null;
-
-    private Double latchedTargetAngle = null;
-    private int lastSeenTag = -1;
-    private int lostCounter = 0;
-    private static final int LOST_LIMIT = 10;
-    
+   
     private static class LimelightConfig {
         String name; double x, y, z, roll, pitch, yaw;
         LimelightConfig(String name, double x, double y, double z, double roll, double pitch, double yaw){
@@ -69,12 +56,11 @@ public class AimAndShoot extends Command {
         this.intake = intake;
         this.outtakeAngle = outtakeAngle;
         this.autoButton = autoButton;
-        this.deadbandDeg = DEAD_BAND;
         this.lights = lights;
 
         addRequirements(drivetrain, intake, outtakeAngle, lights);
-        turnPID.enableContinuousInput(-Math.PI, Math.PI);
 
+        // Apply Robot Offset
         for(LimelightConfig cam : LIMELIGHTS){
             LimelightHelpers.setCameraPose_RobotSpace(cam.name, cam.x, cam.y, cam.z, cam.roll, cam.pitch, cam.yaw);
         }
@@ -83,101 +69,63 @@ public class AimAndShoot extends Command {
     @Override
     public void execute() {
 
-        double omega = 0.0;
-        Pose2d botPose = getAverageBotPose();
+        DriverStation.Alliance alliance = DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue);
+        // Fetches pose relative to far right side of the driverstations on blue alliance
+        LimelightHelpers.PoseEstimate botPose = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-front");
 
-        if(botPose == null && lastKnownPose != null) botPose = lastKnownPose;
-
-        if(botPose != null){
-            lastKnownPose = botPose;
-
-            DriverStation.Alliance alliance = DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue);
-            double hopperX = (alliance==DriverStation.Alliance.Blue)? BLUE_HOPPER_X : RED_HOPPER_X;
-            double hopperY = (alliance==DriverStation.Alliance.Blue)? BLUE_HOPPER_Y : RED_HOPPER_Y;
-	    
-            double dx = hopperX - botPose.getX();
-            SmartDashboard.putNumber("Calculated DX", dx);
-            System.out.println("dx: "+dx);
-            double dy = hopperY - botPose.getY();
-            SmartDashboard.putNumber("Calculated DY", dy);
-            System.out.println("dy: "+dy);
-
-            // Check which tag is currently being found
-            int currentTag = (int) LimelightHelpers.getFiducialID("limelight-front");
-            boolean seesTag = currentTag != -1;
-            SmartDashboard.putNumber("currentFoundTag", currentTag);
-
-            if (seesTag) {
-                lostCounter = 0;
-                if (latchedTargetAngle == null || currentTag != lastSeenTag) {
-                    // Find Angle
-                    latchedTargetAngle = Math.atan2(dy, dx);
-                    lastSeenTag = currentTag;
-                    turnPID.reset();
-                }
-            } else {
-                lostCounter++;
-                if (lostCounter > LOST_LIMIT) {
-                    latchedTargetAngle = null;
-                    lastSeenTag = -1;
-                }
-            }
-
-            if (latchedTargetAngle != null) {
-                double currentAngleRad = botPose.getRotation().getRadians();
-
-                // Find quickest path
-                double error = latchedTargetAngle - currentAngleRad;
-                while(error > Math.PI) error -= 2*Math.PI;
-                while(error < -Math.PI) error += 2*Math.PI;
-
-                SmartDashboard.putNumber("AngleError", error);
-
-                if(Math.abs(Math.toDegrees(error)) > deadbandDeg){
-                    omega = turnPID.calculate(currentAngleRad, latchedTargetAngle);
-                    omega = Math.max(Math.min(omega,3.0),-3.0);
-                } else {
-                    omega = 0;
-                }
-            } else {
-                omega = 0;
-            }
-
-            SmartDashboard.putNumber("omega", omega);
-            System.out.println("Found Omega: " + omega);
-
-            if(autoButton.getAsBoolean() && latchedTargetAngle != null && Math.abs(omega) < 0.05){
-                if (autoIntakeLightsEnabled) {lights.setColor(79, 52, 235);};
-
-                double distanceFeet = Math.hypot(dx, dy)*3.28084;
-                SmartDashboard.putNumber("Distance from Tag", distanceFeet);
-
-                double distanceFactor = Math.min(distanceFeet/SHOOT_DISTANCE,1.0);
-                double scaleFactor = Math.sqrt(distanceFactor);
-
-                double targetOuttakeAngle = ANGLE_MIN + (ANGLE_MAX-ANGLE_MIN)*scaleFactor;
-                targetOuttakeAngle = Math.min(Math.max(targetOuttakeAngle,ANGLE_MIN),ANGLE_MAX);
-
-                double targetSpeed = SPEED_MIN + (SPEED_MAX-SPEED_MIN)*scaleFactor;
-                targetSpeed = Math.min(Math.max(targetSpeed,SPEED_MIN),SPEED_MAX);
-
-                intake.set(targetSpeed);
-                outtakeAngle.setAngle(targetOuttakeAngle);
-            } else {
-                intake.stop();
-                outtakeAngle.stop();
-                if (autoIntakeLightsEnabled) {lights.setColor(11, 222, 0);};
-            }
-            SmartDashboard.putBoolean("IsDetected", true);
-
-        } else {
-            System.out.println("Limelight didn't return data.");
+        if(botPose == null) {
+            omega = 0.4;
+            drivetrain.setControl(m_request.withVelocityX(0).withVelocityY(0).withRotationalRate(omega));
             SmartDashboard.putBoolean("IsDetected", false);
-            if (autoIntakeLightsEnabled) {lights.setColor(250, 75, 43);};
-        }
+            if (autoIntakeLightsEnabled) { lights.setColor(255, 0, 0); }
+        } else {
+            SmartDashboard.putBoolean("IsDetected", true);
+            if (autoIntakeLightsEnabled) { lights.setColor(0, 255, 0); }
 
-        drivetrain.setControl(m_request.withVelocityX(0).withVelocityY(0).withRotationalRate(omega));
-        SmartDashboard.putBoolean("isAutoActive", true);
+            // Set Hopper Pose
+            double hopperX = 0; double hopperY = 0;
+            if (alliance == DriverStation.Alliance.Blue) {
+                hopperX = BLUE_HOPPER_X;
+                hopperY = BLUE_HOPPER_Y;
+                SmartDashboard.putString("Alliance", "BLUE");
+            } else {
+                hopperX = RED_HOPPER_X;
+                hopperY = RED_HOPPER_Y;
+                SmartDashboard.putString("Alliance", "RED");
+            }   
+            SmartDashboard.putNumber("HopperX", hopperX); SmartDashboard.putNumber("HopperY", hopperY);
+
+            // Set Robot Pose
+            double botPoseX = botPose.pose.getX();
+            SmartDashboard.putNumber("botPoseX", botPoseX);
+            double botPoseY = botPose.pose.getY();
+            SmartDashboard.putNumber("botPoseY", botPoseY);
+
+            double botRot = botPose.pose.getRotation().getDegrees();
+
+            // Calculate Rectangular Difference Between Robot and Hopper
+            double dx = hopperX - botPose.pose.getX();
+            double dy = hopperY - botPose.pose.getY();
+
+            double r = Math.sqrt( (dx)*(dx) + (dy)*(dy) );
+            SmartDashboard.putNumber("Distance from Hopper", r);
+
+            // Get Omega in Degrees
+            omega = ((Math.atan2(dy, dx))*(180 / Math.PI));
+            SmartDashboard.putNumber("Omega", omega);
+
+            // kP Speed Control for Omega
+            double appliedOmega = omega * kP;
+
+            // Run Intake if within Deadband
+            if(autoButton.getAsBoolean() && Math.abs(omega - botRot) < DEAD_BAND){
+                intake.set(intakeSpeed);
+            }
+
+            // Apply Drivetrain Control
+            drivetrain.setControl(m_request.withVelocityX(0).withVelocityY(0).withRotationalRate(appliedOmega));
+            SmartDashboard.putBoolean("isAutoActive", true);
+        }
     }
 
     @Override
@@ -190,37 +138,4 @@ public class AimAndShoot extends Command {
 
     @Override
     public boolean isFinished(){ return false; }
-
-    private Pose2d getAverageBotPose() {
-        double sumX = 0, sumY = 0, sumCos = 0, sumSin = 0;
-        int count = 0;
-
-        for(LimelightConfig cam : LIMELIGHTS){
-            if(LimelightHelpers.getTV(cam.name)){
-                Pose2d pose = LimelightHelpers.getBotPose2d_wpiBlue(cam.name);
-                if(pose == null) continue;
-
-                sumX += pose.getX();
-                sumY += pose.getY();
-                double rot = pose.getRotation().getRadians();
-                sumCos += Math.cos(rot);
-                sumSin += Math.sin(rot);
-                count++;
-            }
-        }
-
-        if(count == 0){
-            if (autoIntakeLightsEnabled) { lights.setColor(235, 52, 52); }
-            return null;
-        }
-
-        double avgX = sumX / count;
-        SmartDashboard.putNumber("avgX", avgX);
-        double avgY = sumY / count;
-        SmartDashboard.putNumber("avgY", avgY);
-        double avgRot = Math.atan2(sumSin, sumCos);
-        SmartDashboard.putNumber("avgRot", avgRot);
-
-        return new Pose2d(avgX, avgY, new Rotation2d(avgRot));
-    }
 }
